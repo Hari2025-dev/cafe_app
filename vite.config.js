c02773bbd9c20f9c7129b31086830e44c2556171
Hari2025-dev <prasathmhari@gmail.com>
2024-03-21 16:41:24 +0530

    config commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/cafe_app/",
   plugins: [react()],
-  // define:{
-  //   'process.env.REACT_APP_SUPABASE_URL': JSON.stringify(process.env.VITE_REACT_APP_SUPABASE_URL),
-  //   'process.env.REACT_APP_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_NREACT_APP_SUPABASE_ANON_KEY),
-  // }
 })
